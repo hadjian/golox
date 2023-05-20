8a0feab59fa03428f864630c760e31eecdc0c45c
Pedram Hadjian <pedram.hadjian@siemens.com>
2023-05-21 00:16:28 +0200

    Interpreter can evaluate binary expressions

--- interpreter.go
+++ interpreter.go
@@ -8,7 +8,28 @@ func (i *Interpreter) Evaluate(expr Expr) any {
 }
 
 func (i *Interpreter) VisitBinary(b *Binary) any {
+	left := i.Evaluate(b.Left)
+	right := i.Evaluate(b.Right)
 
+	switch b.Operator.tType {
+	case MINUS:
+		return left.(float64) - right.(float64)
+	case PLUS:
+		if left, ok := left.(float64); ok {
+			if right, ok := right.(float64); ok {
+				return left + right
+			}
+		}
+		if left, ok := left.(string); ok {
+			if right, ok := right.(string); ok {
+				return left + right
+			}
+		}
+	case SLASH:
+		return left.(float64) / right.(float64)
+	case STAR:
+		return left.(float64) * right.(float64)
+	}
 	return nil
 }
 
--- interpreter_test.go
+++ interpreter_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestIsTruthy(t *testing.T) {
 	i := Interpreter{}
@@ -18,6 +21,67 @@ func TestIsTruthy(t *testing.T) {
 			t.Errorf("output  : %v == %v\n", input, out)
 			t.Errorf("expected: %v == %v\n", input, expected)
 			t.Errorf("\n")
+		}
+	}
+}
+
+func TestAddition(t *testing.T) {
+	tests := []struct {
+		left      any
+		operator  Token
+		right     any
+		valueType reflect.Type
+		expected  any
+	}{
+		{
+			4.0,
+			Token{tType: PLUS, lexeme: "+"},
+			5.0,
+			reflect.TypeOf(float64(0.0)),
+			9.0,
+		},
+		{
+			"Hello ",
+			Token{tType: PLUS, lexeme: "+"},
+			"World!",
+			reflect.TypeOf(string("")),
+			"Hello World!",
+		},
+		{
+			4.0,
+			Token{tType: STAR, lexeme: "*"},
+			9.0,
+			reflect.TypeOf(float64(0)),
+			36.0,
+		},
+		{
+			16.0,
+			Token{tType: SLASH, lexeme: "/"},
+			4.0,
+			reflect.TypeOf(float64(0)),
+			4.0,
+		},
+	}
+
+	i := Interpreter{}
+	for _, test := range tests {
+		b := Binary{}
+		b.Left = &Literal{test.left}
+		b.Operator = test.operator
+		b.Right = &Literal{test.right}
+		expr := (&AstPrinter{}).Print(&b)
+
+		output := i.Evaluate(&b)
+		outValue := reflect.ValueOf(output)
+		if outValue.Type().ConvertibleTo(test.valueType) {
+			output := outValue.Convert(test.valueType).Interface()
+			if output != test.expected {
+				t.Errorf("Evaluation failed for binary %s\n", expr)
+				t.Errorf("%v != %v", output, test.expected)
+			}
+		} else {
+			t.Errorf("Evaluation failed for binary %s\n", expr)
+			t.Errorf("Could not cast ouput %v to float64", output)
 		}
 	}
 }
